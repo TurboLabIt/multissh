5435d441272d007a7ccf1755eae710f3f5afcc95
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-26 11:40:35 +0200

    Create base.sh

---
+++ scripts/base.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+REPORT_REMOTE_DIR=/var/log/turbolab.it/
+REPORT_REMOTE_FILE=${REPORT_REMOTE_DIR}multissh-config-collector.csv
+
+REPORT_LOCAL_DIR=/tmp/
+REPORT_LOCAL_FILE=${REPORT_LOCAL_DIR}inventory.csv
