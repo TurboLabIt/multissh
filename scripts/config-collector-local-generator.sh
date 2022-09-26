999d807944627baabea4d839803ba8b49a3bf088
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-26 11:49:58 +0200

    Update config-collector-local-generator.sh

--- scripts/config-collector-local-generator.sh
+++ scripts/config-collector-local-generator.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 echo ""
 
+source "${SCRIPT_DIR}base.sh"
+
 MSSH_REMOTE_LOGIN_USERNAME=$1
 MSSH_REMOTE_HOST=$2
 MSSH_USER_AT_HOST=${MSSH_REMOTE_LOGIN_USERNAME}@${MSSH_REMOTE_HOST}
@@ -10,9 +12,6 @@ function sectionText()
 {
   echo -e "\e[1;33m${1}\e[0m"
 }
-
-REPORT_REMOTE_FILE=/var/log/turbolab.it/multissh-config-collector.csv
-REPORT_LOCAL_FILE=/tmp/inventory.csv
 
 sectionText "Collecting..."
 echo -n "${MSSH_REMOTE_HOST}|${MSSH_TARGET_HOSTS_LOCAL_FILE}|" >> "${REPORT_LOCAL_FILE}"
